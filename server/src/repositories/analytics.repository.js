93a0c88e8a31715cd2c5ddbb1a546f8bd214689b
padhiyarjatin5723-cell <padhiyarjatin5723@gmail.com>
2026-07-09 16:40:48 +0530

    Fix Linux case sensitive imports

--- server/src/repositories/analytics.repository.js
+++ server/src/repositories/analytics.repository.js
@@ -1,4 +1,4 @@
-import Task from "../models/Task.js";
+import Task from "../models/task.js";
 
 export const getAnalyticsData = async (userId) => {
   const tasks = await Task.find({ user: userId });
--- server/src/repositories/user.repository.js
+++ server/src/repositories/user.repository.js
@@ -1,4 +1,4 @@
-import User from "../models/User.js";
+import User from "../models/user.js";
 
 export const getProfileRepo = async (id) => {
   return await User.findById(id).select("-password");
